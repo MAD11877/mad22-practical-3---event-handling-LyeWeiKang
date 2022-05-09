7541883cda34f19f42e27a9c2ca42331ced5eed0
Wei Kang Lye <s10221838@connect.np.edu.sg>
2022-05-10 00:40:47 +0800

    Trying Challenge

--- app/src/main/java/sg/edu/np/mad/mad_practical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad_practical2/MainActivity.java
@@ -39,19 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-        public class User{
-        String name;
-        String description;
-        int id;
-        boolean followed;
-        public User(String n,String d,int i,boolean f){
-            this.name=n;
-            this.description=d;
-            this.id=i;
-            this.followed=f;
-        }
+
 
     }
 
 
-}
